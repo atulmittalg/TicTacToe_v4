b99b2d19a4a48f1db8db91b230186d838856bb41
Atul Mittal <55511902+atulmittalg@users.noreply.github.com>
2020-03-23 00:39:23 +0100

    Refactor: Using class level TicTacToeGame object for test cases.

--- src/test/java/com/game/tictactoe/TicTacToeGameTest.java
+++ src/test/java/com/game/tictactoe/TicTacToeGameTest.java
@@ -1,21 +1,28 @@
 package com.game.tictactoe;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class TicTacToeGameTest {
 
+
+    private TicTacToeGame ticTacToeGame;
+
+    @Before
+    public void initializeTicTacToeGameObject(){
+        ticTacToeGame = new TicTacToeGame();
+    }
+
     @Test
     public void getPlayerAtShouldReturnXWhenFirstTurnIsPlayed() {
-        TicTacToeGame ticTacToeGame = new TicTacToeGame();
         ticTacToeGame.playTurnAt(0,0);
         assertEquals('X', ticTacToeGame.getPlayerAt(0,0));
     }
 
     @Test
     public void getPlayerAtShouldReturnOWhenSecondTurnIsPlayed() {
-        TicTacToeGame ticTacToeGame = new TicTacToeGame();
         ticTacToeGame.playTurnAt(0,0);
         ticTacToeGame.playTurnAt(0,1);
         assertEquals('O', ticTacToeGame.getPlayerAt(0,1));
